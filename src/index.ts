bc8950311a3d1d1a610c22c50476e0f38216e06f
RomanB32777 <e228ea@gmail.com>
2021-02-08 14:53:34 +0000

    modified index.ts: add env configuration

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
 import App from './app'
-//import config from 'config'
+import config from './config/index'
 
-const application: Promise<any> = new App({
-    port: process.env.PORT,
-// config.get('port'),
-    mongoUri: process.env.DB
-// config.get('mongoUri')
-}).run()
-
-if (!application) {
-    console.log('good bye! ');
-    process.exit(1)
+if (config.port && config.mongoUri){
+    const application: Promise<any> = new App({
+        port: +config.port,
+        mongoUri: config.mongoUri
+    }).run()
+    
+    if (!application) {
+        console.log('good bye! ');
+        process.exit(1)
+    }
 }
